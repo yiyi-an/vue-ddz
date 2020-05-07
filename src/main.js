96efa9c3c17e24d1338597f1eae74777cd57988b
安晓东 <xiaodong.an@zhaopin.com.cn>
2020-05-07 17:41:42 +0800

    ws

--- src/main.js
+++ src/main.js
@@ -23,7 +23,8 @@ Vue.use(Barrage)
 Vue.use(new VueSocketIO({
   debug: true,
   // 服务器端地址
-  connection: '//ddz.dodolib.cn',
+  connection: '//123.206.204.35:10068',
+  // connection: '//ddz.dodolib.cn:10068',
   vuex: {
     store
   }
